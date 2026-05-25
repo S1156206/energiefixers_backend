6a6133ea2ce39eb554e4152e21b7288c6963cc99
Lucas van de Pol <s1156206@student.hsleiden.nl>
2026-05-25 16:37:40 +0200

    feat: energy repo

---
+++ backend/src/main/java/com/energiefixers/backend/energy/repository/EnergyReadingRepository.java
@@ -0,0 +1,10 @@
+package com.energiefixers.backend.energy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.energiefixers.backend.energy.models.EnergyReading;
+
+import java.util.List;
+
+public interface EnergyReadingRepository extends JpaRepository<EnergyReading, Long> {
+    List<EnergyReading> findAllByPropertyId(Long propertyId);
+}
