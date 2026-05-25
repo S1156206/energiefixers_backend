e645330cd61b86c2424f1005b2415fa4ed79618b
Lucas van de Pol <s1156206@student.hsleiden.nl>
2026-05-25 16:41:31 +0200

    feat: property repo

---
+++ backend/src/main/java/com/energiefixers/backend/property/repository/PropertyRepository.java
@@ -0,0 +1,11 @@
+package com.energiefixers.backend.property.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.energiefixers.backend.property.models.Property;
+
+public interface PropertyRepository extends JpaRepository<Property, Long> {
+    List<Property> findAllByRegionId(Long id);
+}
