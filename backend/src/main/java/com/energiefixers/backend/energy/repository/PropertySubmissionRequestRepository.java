de7f17a8ad2683aa58fbbaaef736e09e438c6aac
Invictum-Fitness <info@invictum-fitness.com>
2026-06-02 20:03:27 +0200

    feat: added submission request repo

---
+++ backend/src/main/java/com/energiefixers/backend/energy/repository/PropertySubmissionRequestRepository.java
@@ -0,0 +1,12 @@
+package com.energiefixers.backend.energy.repository;
+
+import com.energiefixers.backend.energy.models.PropertySubmissionRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PropertySubmissionRequestRepository extends JpaRepository<PropertySubmissionRequest, Long> {
+    Optional<PropertySubmissionRequest> findByToken(String token);
+    List<PropertySubmissionRequest> findAllByPropertyId(Long propertyId);
+}
