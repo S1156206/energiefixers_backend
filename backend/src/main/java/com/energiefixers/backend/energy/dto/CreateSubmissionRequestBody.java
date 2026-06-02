ffc4513ffdf7be14367df720c0f44573f571392d
Invictum-Fitness <info@invictum-fitness.com>
2026-06-02 20:03:10 +0200

    feat: added submission form request model

---
+++ backend/src/main/java/com/energiefixers/backend/energy/dto/CreateSubmissionRequestBody.java
@@ -0,0 +1,10 @@
+package com.energiefixers.backend.energy.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateSubmissionRequestBody {
+    private String email;
+}
