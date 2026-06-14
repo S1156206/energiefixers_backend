fdcf993e515d2945ef8a371664c84423a7b00191
Invictum-Fitness <info@invictum-fitness.com>
2026-06-14 10:41:19 +0200

    feat: add dashboard controller to disabled auth in security config

--- backend/src/main/java/com/energiefixers/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/energiefixers/backend/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/invitations/*/accept").permitAll()
                         .requestMatchers("/api/submission/**").permitAll()
                         .requestMatchers("/api/unsubscribe/**").permitAll()
+                        .requestMatchers("/api/dashboard/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
